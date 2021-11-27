9800c3acb82607e8fca01f8693d1600dab037ef8
Aleksandr Tsyganok <tsyganok2015@gmail.com>
2021-11-27 15:41:51 +0300

    Added necessary packages for ms sql server, database context, migrations

---
+++ src/NotionV2.API/Models/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace NotionV2.API.Models
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+    }
+}
--- src/NotionV2.API/Startup.cs
+++ src/NotionV2.API/Startup.cs
@@ -6,22 +6,26 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using NotionV2.API.Models;
 
 namespace NotionV2.API
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
             Configuration = configuration;
+            WebHostEnvironment = env;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment WebHostEnvironment { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -31,6 +35,14 @@ namespace NotionV2.API
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "NotionV2.API", Version = "v1"});
             });
+
+            var connection = $"Server={Environment.MachineName};Database=NotionV2Db;";
+            if (WebHostEnvironment.IsDevelopment())
+            {
+                connection += "Trusted_Connection=True;";
+            }
+
+            services.AddDbContext<AppDbContext>(options => options.UseSqlServer(connection));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
