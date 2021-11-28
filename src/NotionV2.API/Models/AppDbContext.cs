6591839673946339df258c340d07b5184e33c75d
Aleksandr Tsyganok <tsyganok2015@gmail.com>
2021-11-28 16:25:25 +0300

    Created user model then added to the database context

--- src/NotionV2.API/Models/AppDbContext.cs
+++ src/NotionV2.API/Models/AppDbContext.cs
@@ -4,6 +4,8 @@ namespace NotionV2.API.Models
 {
     public class AppDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
---
+++ src/NotionV2.API/Models/User.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NotionV2.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        
+        [Required]
+        public string Login { get; set; }
+        
+        [Required]
+        public string Password { get; set; }
+    }
+}
