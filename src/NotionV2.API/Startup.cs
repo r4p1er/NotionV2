fd260381c2a8d8c746b48ca28066b93d2dae0d88
Aleksandr Tsyganok <tsyganok2015@gmail.com>
2021-11-28 18:12:34 +0300

    Added Newtonsoft.Json and Bcrypt packages, pepper string was added to appsettings.Development.json, Startup.cs was set up to use Newtonsoft.Json as default

--- src/NotionV2.API/Startup.cs
+++ src/NotionV2.API/Startup.cs
@@ -30,7 +30,7 @@ namespace NotionV2.API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers().AddNewtonsoftJson();
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo {Title = "NotionV2.API", Version = "v1"});
